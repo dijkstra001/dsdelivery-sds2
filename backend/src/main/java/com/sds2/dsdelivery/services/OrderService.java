4898c91de4ea4bb0cbc3e94d1fc1aff7521522f9
Nathã Correia <ncorreiatrabalhando@gmail.com>
2021-01-04 21:44:19 -0300

    GET /orders

---
+++ backend/src/main/java/com/sds2/dsdelivery/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.sds2.dsdelivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sds2.dsdelivery.dto.OrderDTO;
+import com.sds2.dsdelivery.entities.Order;
+import com.sds2.dsdelivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+
+}
